577091020d2cbd1467602fd1a2c3694eb92d60cc
GravityScriptsV2 <95504918+GravityScriptsV2@users.noreply.github.com>
2025-09-15 15:56:29 -0700

    feat: notify loading screen on first player spawn before shutdown

--- src/client/index.ts
+++ src/client/index.ts
@@ -2,6 +2,12 @@ let playerSpawned = false;
 
 on("playerSpawned", () => {
   if (!playerSpawned) {
+    SendLoadingScreenMessage(
+      JSON.stringify({
+        fullyLoaded: true,
+      })
+    );
+    Wait(5000);
     ShutdownLoadingScreenNui();
     playerSpawned = true;
   }
